e7e60bf789b0b659f455ee4b80ab607c468ea3b0
tran <c12simple@gmail.com>
2017-08-28 16:11:35 +0200

    Test with old pydio password

--- connector/pydio-sql/pydioSqlConnector.go
+++ connector/pydio-sql/pydioSqlConnector.go
@@ -6,19 +6,23 @@ import (
 	"context"
 
 	"fmt"
+	"database/sql"
+	"net/url"
+	"strings"
+	"github.com/coreos/dex/password-hasher"
 )
 
 type Config struct {
-
-	// Sql connection
 	SqlConnection struct {
-		Host string `json:Host`
+		Host 		string `json:Host,omitempty`
+		DBName		string `json:DBName,omitempty`
+		UserName	string `json:UserName,omitempty`
+		Password	string `json:Password,omitempty`
 	}
-
-	UserTableName		string	`json:UserTableName`
-	UserIDColumn		string	`json:UserIDColumn`
-	PasswordColumn		string  `json:PasswordColumn`
-	PasswordEncryptAlgo string	`json:PasswordEncryptAlgo`
+	UserTableName		string	`json:UserTableName,omitempty`
+	UserIDColumn		string	`json:UserIDColumn,omitempty`
+	PasswordColumn		string  `json:PasswordColumn,omitempty`
+	PasswordEncryptAlgo string	`json:PasswordEncryptAlgo,omitempty`
 }
 func (c *Config) Open(logger logrus.FieldLogger) (connector.Connector, error) {
 	return c.OpenConnector(logger)
@@ -49,22 +53,38 @@ var (
 func (p *pydioSQLConnector) Login(ctx context.Context, s connector.Scopes, username, password string) (identity connector.Identity, validPassword bool, err error){
 	p.logger.Printf("Login request for User:%s Password:%s", username, password)
 
-	if username == "username" && password == "P@ssw0rd" {
 
-		identity = connector.Identity{
-			UserID:        "username",
+	localPassword, err := p.getPassword(username)
+
+	passwd := password_hasher.PydioPW{
+		PBKDF2_HASH_ALGORITHM:"sha256",
+		PBKDF2_ITERATIONS  		: 1000,
+		PBKDF2_SALT_BYTE_SIZE 	: 32,
+		PBKDF2_HASH_BYTE_SIZE 	: 24,
+		HASH_SECTIONS 			: 4,
+		HASH_ALGORITHM_INDEX 	: 0,
+		HASH_ITERATION_INDEX 	: 1,
+		HASH_SALT_INDEX 		: 2,
+		HASH_PBKDF2_INDEX 		: 3,
+	}
+
+	ret, err := passwd.CheckDBKDF2PydioPwd(password, localPassword)
+
+	if ret {
+		identity := connector.Identity{
+			UserID:        username,
 			Username:      "User Number 001",
 			Email:         "u001@pydio.com",
 			EmailVerified: true,
 			DisplayName:   "",
-			Roles:         "",
+			Roles:         []string{"defaultRole"},
 			GroupPath:     "",
 			Groups:        []string{},
 			ConnectorData: nil,
 		}
 		return identity, true, nil
 	}
-	return connector.Identity{}, false, fmt.Errorf("User not found")
+	return connector.Identity{}, false, err
 }
 
 
@@ -79,7 +99,7 @@ func (p *pydioSQLConnector) Refresh(ctx context.Context, s connector.Scopes, ide
 			Email:         "u001@pydio.com",
 			EmailVerified: true,
 			DisplayName:   "",
-			Roles:         "",
+			Roles:         []string{"defaultRole"},
 			GroupPath:     "",
 			Groups:        []string{},
 			ConnectorData: nil,
@@ -89,3 +109,53 @@ func (p *pydioSQLConnector) Refresh(ctx context.Context, s connector.Scopes, ide
 	return connector.Identity{}, fmt.Errorf("User not found")
 }
 
+func (p *pydioSQLConnector) getPassword(userid string) (string, error){
+
+	v := url.Values{}
+	set := func(key, val string) {
+		if val != "" {
+			v.Set(key, val)
+		}
+	}
+	set("parseTime", "true")
+	//set("multiStatements", "true")
+	set("collation", "utf8_general_ci")
+	set("charset", "utf8")
+	//set("autocommit", "false")
+
+
+	u := url.URL{
+		Scheme:   "tcp",
+		Host:     "tcp" + "(" + p.Config.SqlConnection.Host + ":3306)",
+		Path:     "/" + p.Config.SqlConnection.DBName,
+		RawQuery: v.Encode(),
+	}
+
+	if p.Config.SqlConnection.UserName != "" {
+		if p.Config.SqlConnection.Password != "" {
+			u.User = url.UserPassword(p.Config.SqlConnection.UserName, p.Config.SqlConnection.Password)
+		} else {
+			u.User = url.User(p.Config.SqlConnection.UserName)
+		}
+	}
+	replaceStr := "tcp" + "://"
+	dns := strings.TrimPrefix(u.String(), replaceStr)
+	db, err := sql.Open("mysql", dns)
+	defer db.Close()
+	if err != nil {
+		return "", err
+	}
+
+	stmt, err := db.Prepare("SELECT " + p.Config.PasswordColumn + " FROM " + p.Config.UserTableName + " WHERE " + p.Config.UserIDColumn + " = ?")
+	if err != nil{
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	res := stmt.QueryRow(userid)
+	defer stmt.Close()
+	var password string
+	err = res.Scan(&password)
+
+	return password, nil
+}
